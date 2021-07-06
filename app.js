6e5987c3ade5467b94f37c6327d1329895cb6a23
Vaibhav Shukla <shuklavaibhav0306@gmail.com>
2021-07-07 01:06:01 +0530

    updated callbackurl

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ const transporter = nodemailer.createTransport({
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets"
+        callbackURL: process.env.CALLBACK_URL
     },
     async function (accessToken, refreshToken, profile, cb) {
         //console.log("--------------------------------------Profile----------------------------------------------------");
